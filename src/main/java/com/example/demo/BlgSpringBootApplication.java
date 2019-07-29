21fc188da60b8dd4d3f2ea4dfea95b9861fb46c1
tatsunoriuehara <uehara.t29@gmail.com>
2019-07-29 09:11:44 +0900

    first commit

---
+++ src/main/java/com/example/demo/BlgSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlgSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlgSpringBootApplication.class, args);
+	}
+
+}
